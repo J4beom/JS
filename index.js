a1cc090e8ee2104302e683d2fff9c031f2d64c3a
khu45j123 <khu45j123@naver.com>
2016-09-11 14:09:35 +0900

    Update and rename hello.js to index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+alert('어서오세요 !');
+function validateForm()
+{
+var x=document.forms["login"]["username"].value;
+if (x==null || x=="")
+  {
+  alert("Please fill out the username");
+  return false;
+  }
+}
